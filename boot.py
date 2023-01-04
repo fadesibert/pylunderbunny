4a421fdd1a80dd73701b9a2fd5a528e2339639a9
Freddie de Sibert <freddie@revmo.app>
2023-01-04 19:36:38 +0000

    fix WIFI connection code and print whether it's connected once booted

--- boot.py
+++ boot.py
@@ -5,10 +5,10 @@ SSID = "YOUR_SSID_HERE"
 KEY = "YOUR_KEY_HERE"
 
 sta_if = network.WLAN(network.STA_IF)
-if not sta_if.is_connected():
+if not sta_if.isconnected():
     print(f"Connecting to network {SSID}...")
     sta_if.active(True)
     sta_if.connect(SSID, KEY)
-    while not sta_if.is_connected():
+    while not sta_if.isconnected():
         sys.stdout.write(".")
     print("Connected!")
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
 import network
 
 # Micropython uses a main loop by convention
 # rather than a if __name__ == '__main__'
 
 while True:
+    nic = network.WLAN()
+    print(f"{nic.isconnected()=}")
     time.sleep(1)
